2f87d8e88cf78a946a4cb8bb45614259c36a8db5
DamienBrn <damien.berno@outlook.com>
2019-06-30 11:48:31 +0200

    Main functions added

--- Components/Timer.js
+++ Components/Timer.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TouchableOpacity, Switch} from 'react-native'
+import Vibrate from '../utils/vibrate'
 
 
 export default class Timer extends React.Component{
@@ -8,18 +8,63 @@ export default class Timer extends React.Component{
 
     constructor(props){
         super(props)
+        coutDown = undefined,
+        timeOptions = {
+            five :5,
+            twentyFive : 25
+        }
+        chosenTimeInMinutes = 5,
+
         this.state={
-            timerMinutesValue:0,
-            timerSecondsValue:20,
+            timerMinutesValue : chosenTimeInMinutes,
+            timerSecondsValue : 0,
+            timerIsActive : false,
         }
     }
 
     render(){
         return(
             <View>
-                <Text style={styles.timerValue}>{this.displayTwoDigits(this.state.timerMinutesValue)} : {this.displayTwoDigits(this.state.timerSecondsValue)}</Text>
+
+                <View style={styles.buttons_container}>
+                    <TouchableOpacity onPress={()=>this.changeChosenTime(timeOptions.five)} disabled={this.state.timerIsActive}>
+                        <Text style={[styles.button_format, styles.button_colors, chosenTimeInMinutes !== timeOptions.five ? styles.disabled_button : null ]}>
+                            {timeOptions.five} min
+                        </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={()=>this.changeChosenTime(timeOptions.twentyFive)} disabled={this.state.timerIsActive}>
+                        <Text style={[styles.button_format, styles.button_colors, chosenTimeInMinutes !== timeOptions.twentyFive ? styles.disabled_button : null ]}>
+                            {timeOptions.twentyFive} min
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+
+                <Text style={styles.timer_value}>{this.displayTwoDigits(this.state.timerMinutesValue)} : {this.displayTwoDigits(this.state.timerSecondsValue)}</Text>
+            
+                <View style={styles.buttons_container}>
+                    <TouchableOpacity title="Start" activeOpacity={this.state.timerIsActive ? 1 : 0.7} onPress={()=>this.startTimer()} disabled={this.state.timerIsActive}>
+                        <Text style={[styles.button_format , styles.button_colors, this.state.timerIsActive ? styles.disabled_button : null ]} >Start</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity title="Pause" onPress={()=>this.pauseTimer()}>
+                        <Text style={[styles.button_format , styles.button_colors, !this.state.timerIsActive ? styles.disabled_button : null ]} >Pause</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity title="Reset" onPress={()=>this.resetTimer()} disabled={this.state.timerIsActive}>
+                        <Text style={[styles.button_format , styles.button_colors, this.state.timerIsActive ? styles.disabled_button : null ]} >Reset</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
+    }
+
+    changeChosenTime(chosenTime){
+        this.setState({
+            timerMinutesValue : chosenTime,
+            timerSecondsValue : 0
+        })
+        chosenTimeInMinutes = chosenTime
     }
 
 
@@ -27,13 +72,28 @@ export default class Timer extends React.Component{
         return (numberOfDigits < 10) ? ('0' + numberOfDigits) : numberOfDigits
     }
 
-    componentDidMount(){
-        this.decrementTimer()
+
+    startTimer(){
+        if(!this.state.timerIsActive){
+            this.setState({timerIsActive : true})
+            this.decrementTimer()
+        }
     }
 
+    pauseTimer(){
+        this.setState({timerIsActive : false})
+        clearInterval(coutDown)
+    }
+
+    resetTimer(){
+        this.setState({
+            timerMinutesValue : chosenTimeInMinutes,
+            timerSecondsValue : 0
+        })
+    }
 
     decrementTimer(){
-        const timer = setInterval(()=>{
+        coutDown = setInterval(()=>{
             this.setState((previousState)=>{
                 return {
                     timerSecondsValue : this.decrementSeconds(previousState.timerSecondsValue - 1),
@@ -41,7 +101,7 @@ export default class Timer extends React.Component{
                 }
              })
 
-             this.checkIfTimerHasEnded()
+             this.checkIfTimerHasEnded(coutDown)
 
         },1000)
     }
@@ -56,14 +116,12 @@ export default class Timer extends React.Component{
     }
 
 
-    checkIfTimerHasEnded(){
+    checkIfTimerHasEnded(coutDown){
         if(this.state.timerMinutesValue === 0 && this.state.timerSecondsValue === 0){
-            clearInterval(timer)
+            clearInterval(coutDown)
+            Vibrate()
         }
     }
-
-
-
 
 }
 
@@ -71,7 +129,28 @@ export default class Timer extends React.Component{
 
 const styles = StyleSheet.create({
 
-    timerValue:{
-        fontSize:72
+    timer_value : {
+        fontSize : 72,
+    },
+
+    buttons_container : {
+        flexDirection : 'row',
+        justifyContent : 'space-between',
+        alignItems : 'center'
+    },
+
+    button_colors : {
+        color : '#ffffff',
+        backgroundColor :'#2970e3',
+    },
+
+    button_format : {
+        fontSize : 20,
+        padding : 5,
+        borderRadius : 5,
+    },
+
+    disabled_button : {
+        backgroundColor : 'rgba(41, 112, 227, 0.2)',
     }
 })
